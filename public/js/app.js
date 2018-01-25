71ccf612cd6bfb641554e55981009318425fa36c
madsray <30870145+madsray@users.noreply.github.com>
2018-01-25 16:48:21 -0600

    categories are showing and bars are popping up in console:

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,12 @@
 const app = angular.module('happyhour', []);
 app.controller('MainController', ['$http', function($http){
   this.cities = [];
+  this.cats = [];
+  this.showCatPage = false;
+  this.showCityPage = true;
+  this.cityID = 0;
+  this.catsID = 0;
+  this.findBars = [];
 
   this.getAllCities = () => {
     $http({
@@ -10,9 +16,51 @@ app.controller('MainController', ['$http', function($http){
     }).then(response=> {
       console.log(response.data);
       this.cities = response.data;
+
     }).catch(reject => {
       console.log('reject: ', reject)
     });
   };
   this.getAllCities();
+
+    this.showCategories = (city) => {
+      console.log(this.cityID)
+      this.cityID = city.id
+      $http({
+        method: 'GET',
+        url: 'http://localhost:3000/cities/' + this.cityID
+      }).then(response => {
+        this.cats = response.data.categories;
+        console.log(this.cats)
+        this.categories =
+        // console.log(this.oneMovie);
+
+        this.showCatPage = true;
+        this.showCityPage = false;
+      }).catch(err => console.log(err));
+    };
+
+      this.showBars = (cat) => {
+        console.log(this.cityID)
+        console.log(cat.id)
+        this.cityID
+        this.catsID
+        $http({
+          method: 'GET',
+          url: 'http://localhost:3000/' + 'cities/' + this.cityID + '/categories/' + cat.id
+        }).then(response => {
+          console.log("DO SOMETHING")
+          // this.bars = response.data.categories;
+          this.findBars = response.data;
+          console.log(this.findBars);
+          this.showCatPage = true;
+          this.showCityPage = false;
+        }).catch(err => console.log(err));
+      };
+
+
+
+
+
+
 }]);
